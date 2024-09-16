fd5e70f80542b9b444c98feb4098d0c9b851a348
DustVoyager <ohjamdol@gmail.com>
2024-09-16 15:03:25 +0900

    seo setting

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -5,6 +5,7 @@ import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
 import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+import { Metadata } from "next";
 
 // export const dynamic = "force-dynamic";
 // 특정 페이지의 유형을 강제로 Static, Dynamic 페이지로 설정
@@ -54,6 +55,16 @@ async function RecoBooks() {
 }
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "한입북스",
+  description: "한입에 쏙 들어오는 도서 추천 서비스",
+  openGraph: {
+    title: "한입북스",
+    description: "한입에 쏙 들어오는 도서 추천 서비스",
+    images: ["/thumbnail.png"],
+  },
+};
 
 export default function Home() {
   return (
